b4f1d345d2c1b11fb03bb09d86c78017ad1b60a0
Phong Nguyen <phong99tb@gmail.com>
2023-10-25 23:46:32 +0700

    add antd

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter } from "react-router-dom";
 import Router from './router/router'
+import './index.css';
 
 
 function App() {
--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { DatePicker, message } from 'antd';
 
-const index = () => {
-    return (
-        <div>About</div>
-    )
+
+const About = () => {
+    const [date, setDate] = useState<any>(null);
+  const [messageApi, contextHolder] = message.useMessage();
+  const handleChange = (value:any) => {
+    messageApi.info(`Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`);
+    setDate(value);
+  };
+  return (
+    <div style={{ width: 400, margin: '100px auto' }}>
+      <DatePicker onChange={handleChange} />
+      <div style={{ marginTop: 16 }}>
+        Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
+      </div>
+      {contextHolder}
+    </div>
+  );
 }
 
-export default index
+export default About
